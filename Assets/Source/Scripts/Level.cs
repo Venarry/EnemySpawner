a4ecc1a7b95d9ee48a8e4343c1ad429a8348a22f
Venarry <venarry@mail.ru>
2023-03-21 15:26:14 +0300

    FixText

--- Assets/Source/Scripts/Level.cs
+++ Assets/Source/Scripts/Level.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Level : MonoBehaviour // *для ментора* - хотел попытаться реализовать код с точкой входа для избежания ошибок при спавне игрока. Если возможно, хотел бы получить фитбек по этой реализации
+public class Level : MonoBehaviour
 {
     [SerializeField] private EnemySpawner _spawner;
     [SerializeField] private Player _playerTemplate;
